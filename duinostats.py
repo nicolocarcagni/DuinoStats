75335049d1bf02e4855202a74f0b21ae5677ff8c
Niko <nicolo.carcagni@mail.com>
2022-12-05 22:02:15 +0100

    Update
    New features:
     - Change username without restart the script
     - API overloading protection

--- duinostats.py
+++ duinostats.py
@@ -11,10 +11,15 @@ class bcolors:
     BOLD = '\033[1m'
 
 clear = lambda: os.system('clear') # os.system('cls') on Windows Systems
-client = DuinoClient()
-username = input('Username: ')
+client = DuinoClient()\
+
+clear()
+
 c = 'r'
 e = 0
+r = 0
+
+username = input('Username: ')
 while c != 'q':
     clear()
     try:
@@ -27,13 +32,37 @@ while c != 'q':
         print(bcolors.OKGREEN + username + "'s miners: " + bcolors.ENDC)
         for miner in result.miners:
             print('- ' + miner.identifier + ': ' + str(miner.hashrate) + ' H/s')
-        c = input(bcolors.WARNING + '(r)efresh / (q)uit: ' + bcolors.ENDC)
+        c = input(bcolors.WARNING + '(r)efresh / (u)sername / (q)uit: ' + bcolors.ENDC)
+        
+        if c == 'r':
+            r = r + 1
+            if r == 2:
+                print('')
+                print(bcolors.WARNING + 'To avoid API overloading, stop refreshing for a while!' + bcolors.ENDC)
+                sleep(3)
+            elif r == 3:
+                print('')
+                print(bcolors.FAIL + 'To avoid a BAN, stop refreshing for a while!' + bcolors.ENDC)
+                sleep(35)
+                r = 0
+
+        if c == 'u':
+            clear()
+            username = input('Username: ')
 
 if e != 0:
+    print(bcolors.FAIL + 'Error ', end='')
     if e == 100:
-        print(bcolors.FAIL + 'Error 100: Check that the entered data is correct' + bcolors.ENDC)
-        sleep(3)
+        print('100: Check username or network')
+        print(bcolors.WARNING + 'If the error persists check server status at: ', end='')
+        print('https://status.duinocoin.com/' + bcolors.ENDC)
+        sleep(4)
+    else:
+        print('Please try again in a while...' + bcolors.ENDC)
+        sleep(0.5)
+        print(bcolors.WARNING + 'If the error persists check server status: ', end='')
+        print('https://status.duinocoin.com/' + bcolors.ENDC)
+        sleep(4)
 
 clear()
-print(bcolors.BOLD + 'Developed by iTzNikolovich' + bcolors.ENDC)
-        
+print(bcolors.BOLD + 'Developed with ❤️  by iTzNikolovich' + bcolors.ENDC)
